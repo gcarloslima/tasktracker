e1a4f1fb178c82f2aa4cd59202b356b38c4418a7
gcarloslima <carlosrelgs@gmail.com>
2026-01-08 07:24:46 -0300

    Argument parser for adding tasks

---
+++ README.md
@@ -0,0 +1,32 @@
+# Task Tracker
+
+A CLI tool for task management
+
+## Installation
+
+```
+uv sync
+```
+```
+uv pip install -e .
+```
+
+
+### For Linux/Mac
+```
+source .venv/bin/activate
+```
+### For Windows
+```
+.venv/Scripts/activate
+```
+
+## Usage
+
+```
+task-cli add "Buy groceries"
+```
+
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from argparse import ArgumentParser
+
 def main():
-    print("Hello from tasktracker!")
+    parser = ArgumentParser(
+        prog="task-cli", description="A command-line tool for managing tasks."
+    )
+    
+    subparsers = parser.add_subparsers(dest="command")
+    add_parser = subparsers.add_parser("add")
+    add_parser.add_argument("title")
+    args = parser.parse_args()
+    print(args)
 
 
 if __name__ == "__main__":
