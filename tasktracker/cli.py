dd431919a4f26516a027238112d22f4044cdf808
gcarloslima <carlosrelgs@gmail.com>
2026-01-08 07:44:23 -0300

    Refactor cli to its respective file and create a function to build the parser

--- tasktracker/cli.py
+++ tasktracker/cli.py
@@ -1,14 +1,21 @@
 from argparse import ArgumentParser
 
-def main():
+
+def _build_parser() -> ArgumentParser:
     parser = ArgumentParser(
         prog="task-cli", description="A command-line tool for managing tasks."
     )
-    
+
     subparsers = parser.add_subparsers(dest="command")
     add_parser = subparsers.add_parser("add")
     add_parser.add_argument("title")
-    args = parser.parse_args()
+
+    return parser
+
+
+def main(argv=None):
+    parser = _build_parser()
+    args = parser.parse_args(argv)
     print(args)
 
 
